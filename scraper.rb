c493959af1ebb80be083b83f99836ac0f429c447
Tony Bowden <tony@tmtm.com>
2016-03-22 06:13:14 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,20 +1,8 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'rest-client'
 
-@pages = [
-  'Category:Members of the House of Representatives of the Philippines'
-]
-
-@pages.map { |c| WikiData::Category.new(c, 'en').wikidata_ids }.flatten.uniq.each_with_index do |id, i|
-  puts i if (i % 100).zero?
-  data = WikiData::Fetcher.new(id: id).data('en') or next
-  # puts "%s %s" % [data[:id], data[:name]]
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+names = WikiData::Category.new('Category:Members of the House of Representatives of the Philippines', 'es').member_titles
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
 
