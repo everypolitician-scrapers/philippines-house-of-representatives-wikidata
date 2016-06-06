3fce656c7d15e65614273e20e610d24b6c1e7b4c
Tony Bowden <tony@tmtm.com>
2016-06-06 10:12:41 +0100

    Use Wikipedia page list directly
    The Categories have all been reorganised. Simpler to just get from the
    list page.

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,19 @@
 
 require 'wikidata/fetcher'
 
-names = WikiData::Category.new('Category:Members of the House of Representatives of the Philippines', 'en').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+district = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/16th_Congress_of_the_Philippines',
+  after: '//span[@id="District_representatives"]',
+  before: '//span[@id="Party-list_representatives"]',
+  xpath: '//table[.//th[.="Representative"]]//td[position() = last() - 3]//a[not(@class="new")]/@title',
+) 
+
+party_list = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/16th_Congress_of_the_Philippines',
+  after: '//span[@id="Party-list_representatives"]',
+  before: '//span[@id="Current_composition_2"]',
+  xpath: '//table[.//th[.="Representative"]]//td[position() = last() - 2]//a[not(@class="new")]/@title',
+) 
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: district | party_list })
 
