e77d7da5e581215ac4d8291561afc85092a14dd2
Tony Bowden <tony@tmtm.com>
2016-09-16 15:21:25 +0100

    Make //tr// explicit

--- scraper.rb
+++ scraper.rb
@@ -7,15 +7,15 @@ district = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/16th_Congress_of_the_Philippines',
   after: '//span[@id="District_representatives"]',
   before: '//span[@id="Party-list_representatives"]',
-  xpath: '//table[.//th[.="Representative"]]//td[position() = last() - 3]//a[not(@class="new")]/@title',
-) 
+  xpath: '//table[.//th[.="Representative"]]//tr//td[position() = last() - 3]//a[not(@class="new")]/@title',
+)
 
 party_list = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/16th_Congress_of_the_Philippines',
   after: '//span[@id="Party-list_representatives"]',
   before: '//span[@id="Current_composition_2"]',
-  xpath: '//table[.//th[.="Representative"]]//td[position() = last() - 2]//a[not(@class="new")]/@title',
-) 
+  xpath: '//table[.//th[.="Representative"]]//tr//td[position() = last() - 2]//a[not(@class="new")]/@title',
+)
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: district | party_list })
 
