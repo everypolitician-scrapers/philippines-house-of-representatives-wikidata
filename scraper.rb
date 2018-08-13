b2b360d2ab52d5b8bdb30a895a9101d6acc29f04
Tony Bowden <tony@tmtm.com>
2018-08-13 14:43:49 +0100

    Find text in header

--- scraper.rb
+++ scraper.rb
@@ -7,28 +7,28 @@ district_16 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/16th_Congress_of_the_Philippines',
   after: '//span[@id="District_representatives"]',
   before: '//span[@id="Party-list_representatives"]',
-  xpath: '//table[.//th[.="Representative"]]//tr//td[position() = last() - 3]//a[not(@class="new")]/@title',
+  xpath: '//table[.//th[contains(.,"Representative")]]//tr//td[position() = last() - 3]//a[not(@class="new")]/@title',
 )
 
 district_17 = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/17th_Congress_of_the_Philippines',
   after: '//span[@id="District_representatives"]',
   before: '//span[@id="Party-list_representatives"]',
-  xpath: '//table[.//th[.="Representative"]]//tr//td[position() = last() - 3]//a[not(@class="new")]/@title',
+  xpath: '//table[.//th[contains(.,"Representative")]]//tr//td[position() = last() - 3]//a[not(@class="new")]/@title',
 )
 
 party_list_16 = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/16th_Congress_of_the_Philippines',
   after: '//span[@id="Party-list_representatives"]',
   before: '//span[@id="Current_composition_2"]',
-  xpath: '//table[.//th[.="Representative"]]//tr//td[position() = last() - 2]//a[not(@class="new")]/@title',
+  xpath: '//table[.//th[contains(.,"Representative")]]//tr//td[position() = last() - 2]//a[not(@class="new")]/@title',
 )
 
 party_list_17 = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/17th_Congress_of_the_Philippines',
   after: '//span[@id="Party-list_representatives"]',
   before: '//span[@id="References"]',
-  xpath: '//table[.//th[.="Representative"]]//tr//td[position() = last() - 2]//a[not(@class="new")]/@title',
+  xpath: '//table[.//th[contains(.,"Representative")]]//tr//td[position() = last() - 2]//a[not(@class="new")]/@title',
 )
 
 # Find all P39s of the Legislature
